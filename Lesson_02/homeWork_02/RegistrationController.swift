0986d55fce03f236bc984c6041db339204513e34
Kirill <kirillmsc@gmail.com>
2019-04-10 18:19:32 +0300

    Add lesson 2

--- Lesson_02/homeWork_02/RegistrationController.swift
+++ Lesson_02/homeWork_02/RegistrationController.swift
@@ -1,5 +1,5 @@
 //
-//  registrationControllerViewController.swift
+//  RegistrationController.swift
 //  homeWork_02
 //
 //  Created by k.kochemasov on 07/04/2019.
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class registrationControllerViewController: UIViewController {
+class RegistrationController: UIViewController {
 
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var mailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var rePasswordInput: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var doneButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
