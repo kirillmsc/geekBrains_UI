3be8aae3e2fcd2872615355526a6f2568f15555c
Kirill <kirillmsc@gmail.com>
2019-04-10 18:44:49 +0300

    Add lesson 3

---
+++ Lesson_03/homeWork_03/Cells/FriendsCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsCell.swift
+//  homeWork_03
+//
+//  Created by k.kochemasov on 10/04/2019.
+//  Copyright © 2019 k.kochemasov. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    
+    static let reuseId = "FriendsCell"
+
+    @IBOutlet var friendsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ Lesson_03/homeWork_03/Cells/GroupsCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroupsCell.swift
+//  homeWork_03
+//
+//  Created by k.kochemasov on 10/04/2019.
+//  Copyright © 2019 k.kochemasov. All rights reserved.
+//
+
+import UIKit
+
+class GroupsCell: UITableViewCell {
+    
+    static let reuseId = "GroupsCell"
+    
+    @IBOutlet var groupsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
