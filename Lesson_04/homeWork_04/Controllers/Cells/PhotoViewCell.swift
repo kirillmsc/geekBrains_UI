d8447ba85f28aab712061c31f180358d6cbedf25
Kirill <kirillmsc@gmail.com>
2019-04-28 14:27:58 +0300

    Add photo animation

--- Lesson_04/homeWork_04/Controllers/Cells/PhotoViewCell.swift
+++ Lesson_04/homeWork_04/Controllers/Cells/PhotoViewCell.swift
@@ -15,4 +15,26 @@ class PhotoViewCell: UICollectionViewCell {
     @IBOutlet var iconImageView: UIImageView!
     @IBOutlet var likeCount: UILabel!
     
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIImageView.animate(withDuration: 0.5,
+                            delay: 0,
+                            usingSpringWithDamping: 0.8,
+                            initialSpringVelocity: 0.2,
+                            animations: {
+                                self.iconImageView.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        }, completion: nil)
+        self.iconImageView.startAnimating()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIImageView.animate(withDuration: 1,
+                            delay: 0.2,
+                            usingSpringWithDamping: 0.3,
+                            initialSpringVelocity: 0.4,
+                            animations: {
+                                self.iconImageView.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
+        }, completion: nil)
+        self.iconImageView.startAnimating()
+    }
 }
